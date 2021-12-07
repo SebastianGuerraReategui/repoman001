46a8b207c9d236b9517a731fbf33b1228d7177b6
SebastianGuerraReategui <92555644+SebastianGuerraReategui@users.noreply.github.com>
2021-12-06 20:52:20 -0500

    js parte2

--- entregaClase4.js
+++ entregaClase4.js
@@ -17,7 +17,7 @@ if(pago){
 if(cuotas == "Sí"){
     operacion = true
 }else{
-    alert("Tu cuenta es de " + primerTotal)
+    document.write("Tu cuenta es de " + primerTotal)
     operacion = false
 }
 if(operacion){
@@ -34,8 +34,41 @@ if(cuotasOperacion <=24){
     finalCuotas = false
 }
 if(finalCuotas)
-    document.write(Text= "Tu cuenta será de " + cuotasOperacion + " cuotas de" + primerTotal/cuotasOperacion + " soles cada una");
+    document.write(Text= "Tu cuenta será de " + cuotasOperacion + " cuotas de " + primerTotal/cuotasOperacion + " soles cada una ");
 return precioBrutoCalc;
 }
 
-let precioBruto2 = precioBase(precioBruto);
+let precioBruto2 = precioBase(precioBruto)
+
+/*Objeto cliente*/
+
+
+class Cliente{
+    constructor(nom, dni, tarjeta) {
+        (this.nom = nom),
+        (this.dni = dni),
+        (this.tarjeta = tarjeta)
+    }
+
+
+}
+let nomP = prompt("Inserte su nombre:")
+let dniP = prompt("Inserte su DNI");
+while(isNaN(dniP)){
+    dniP = prompt("Ingrese un DNI válido")
+    }
+    while(dniP>99999999){
+        dniP = prompt("Ingrese un DNI válido")
+        } 
+let tarjeta = prompt("Inserte el número de su tarjeta")
+while(isNaN(tarjeta)){
+    dniP = prompt("Ingrese una tarjeta de crédito válida")
+    }
+
+const usuario = new Cliente(nomP, dniP, tarjeta, "blanco", 12.2, "Catti")
+
+console.log (usuario);
+
+for (let atributos in usuario){
+    document.write(usuario[atributos]);
+}
